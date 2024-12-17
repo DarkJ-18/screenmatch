17fe623c37720d0340d07bea5c9e86ea336ecd95
DarkJ-18 <j.juancamilojurado@gmail.com>
2024-12-17 00:54:25 -0500

    Se sube proyecto Java

---
+++ src/Principal.java
@@ -0,0 +1,37 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
+
+       miPelicula.muestraFichaTecnica();
+       miPelicula.evalua(10);
+       miPelicula.evalua(10);
+       miPelicula.evalua(7.8);
+
+
+        System.out.println("El total de evaluaciones es : " + miPelicula.getTotalDelasEvaluaciones());
+        System.out.println("La media de las evaluaciones es : " + miPelicula.calculaMedia());
+
+        Serie casaDragon = new Serie();
+        casaDragon.setNombre("La casa del dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpidodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println("Duracion total de la serie : " + casaDragon.getDuracionEnMinutos());
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(casaDragon);
+        System.out.println(calculadora.getTiempoTotal());
+
+    }
+}
